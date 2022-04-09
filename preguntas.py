e832ee037ab16999edf6d0b48edef7aef472685b
yavelasquezo <yavelasquezo@unal.edu.co>
2022-04-08 19:01:10 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -185,19 +185,6 @@ def pregunta_09():
 
 
 def pregunta_10():
-    """
-    Construya una tabla que contenga _c1 y una lista separada por ':' de los valores de
-    la columna _c2 para el archivo `tbl0.tsv`.
-
-    Rta/
-                                   _c1
-      _c0
-    0   A              1:1:2:3:6:7:8:9
-    1   B                1:3:4:5:6:8:9
-    2   C                    0:5:6:7:9
-    3   D                  1:2:3:5:5:7
-    4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
-    """
     l=tbl0.copy()
     l['_c22']=l['_c2'].apply(lambda x: str(x))
     def cadena(df):
@@ -211,9 +198,21 @@ def pregunta_10():
     f=list(y)
     #ahora creo una tabla con estas columnas
     df = pd.DataFrame(list(zip(p,f)), columns = ['_c1','_c2'])
-    
-
     return df
+    """
+    Construya una tabla que contenga _c1 y una lista separada por ':' de los valores de
+    la columna _c2 para el archivo `tbl0.tsv`.
+
+    Rta/
+                                   _c1
+      _c0
+    0   A              1:1:2:3:6:7:8:9
+    1   B                1:3:4:5:6:8:9
+    2   C                    0:5:6:7:9
+    3   D                  1:2:3:5:5:7
+    4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
+    """
+    
 
 
 def pregunta_11():
